82dd8b67684d752362621cd30b5894dd32cc2fbe
Jaeseob Lee <Jaeseob.Lee@deliveryhero.co.kr>
2020-01-17 15:47:21 +0900

    폴더 구조

---
+++ SimpleTodo/ViewController/MemoList/MemoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoListViewController.swift
+//  SimpleTodo
+//
+//  Created by 이재섭(Lee, JaeSeob) on 17/01/2020.
+//  Copyright © 2020 leftydev. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
