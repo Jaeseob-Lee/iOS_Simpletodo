040b9c45f34f2180b61917f9758f714fa73eab72
Jaeseob Lee <Jaeseob.Lee@deliveryhero.co.kr>
2020-01-17 15:31:39 +0900

    make project

---
+++ SimpleTodo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SimpleTodo
+//
+//  Created by 이재섭(Lee, JaeSeob) on 17/01/2020.
+//  Copyright © 2020 leftydev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SimpleTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTodo
+//
+//  Created by 이재섭(Lee, JaeSeob) on 17/01/2020.
+//  Copyright © 2020 leftydev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
